a15cccbde4974a69b0031f712762a9cf6d3d11ab
xeliot <daveho8888@gmail.com>
2017-05-12 22:57:44 -0700

    finish beta test 1

--- string_transform.py
+++ string_transform.py
@@ -7,26 +7,43 @@ def print_matrix(matrix):
         print ar
 
 def translate(str1, str2):
+
+    #INITIALIZE MATRIX ACCORDING TO STRING 1 and STRING 2 LENGTH
     matrix = []
     for i in range(0, len(str1)):
         tmp = []
         for j in range(0, len(str2)):
             tmp.append(None)
         matrix.append(tmp)
-    
+    #INITALIZE FIRST ROW AND FIRST COLUMN
     if str1[0] == str2[0]:
         matrix[0][0] = 0
     else:
         matrix[0][0] = 1
-    
     for i in range(1, len(str2)):
         matrix[0][i] = matrix[0][i-1] + 1
-    
     for i in range(1, len(str1)):
         matrix[i][0] = matrix[i-1][0] + 1
+    #FILL IN THE BOARD
+    for i in range(1, len(str1)):
+        for j in range(1, len(str2)):
+            possible = []
+            if str1[i] == str2[j]:
+                possible.append(matrix[i-1][j-1])
+            else:
+                possible.append(matrix[i-1][j-1]+1)
+            possible.append(matrix[i][j-1]+1)
+            possible.append(matrix[i-1][j]+1)
+            matrix[i][j]=min(possible)
 
     print_matrix(matrix)
 
-str1 = "sink"
-str2 = "shiny"
-translate(str1, str2)
+    return matrix[len(str1)-1][len(str2)-1]
+
+#str1 = "sink"
+#str2 = "shiny"
+
+str1 = "suitable"
+str2 = "situation"
+distance = translate(str1, str2)
+print distance
